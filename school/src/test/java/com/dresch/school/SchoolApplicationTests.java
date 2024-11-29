c30f77addb15f8ff86d688b949292f5fb9fa6753
AlexandreDresch <connectWithAlexandre@proton.me>
2024-11-28 23:37:17 -0300

    feat: add school microservice

---
+++ school/src/test/java/com/dresch/school/SchoolApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dresch.school;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SchoolApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
